a5b87936917b7e5161ada7a9026b4122cf4cab4e
samaelleo <76465437+samaelleo@users.noreply.github.com>
2024-03-13 15:16:48 +0330

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -52,7 +52,7 @@ docker rm -f firefox
 docker rm -f 3x-ui
 docker restart socks5
 sudo docker-compose up -d
-sudo apt install ufw -y
+echo "y" | sudo apt install ufw
 sudo ufw default allow routed
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
