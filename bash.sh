47e2ba7faede45b58db71ed7479e53961f5958af
samaelleo <76465437+samaelleo@users.noreply.github.com>
2024-03-14 14:16:04 +0330

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -78,6 +78,5 @@ sudo ufw deny out from any to 192.168.0.0/16
 sudo ufw deny out from any to 141.101.78.0/23
 sudo ufw deny out from any to 173.245.48.0/20
 echo "y" | sudo ufw enable
-tmux
 sudo apt upgrade -y
 exit
