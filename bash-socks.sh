eaf371882c60ea05a514ddabc25ba49710c1fc32
samaelleo <76465437+samaelleo@users.noreply.github.com>
2024-06-01 11:58:22 +0330

    Update bash-socks.sh

--- bash-socks.sh
+++ bash-socks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "ubuntu:asdfASDF1234!" | sudo chpasswd
 echo "root:asdfASDF1234!" | sudo chpasswd
-sudo sed -i '1s/^/nameserver 1.1.1.1\n/' /etc/resolv.conf
+#sudo sed -i '1s/^/nameserver 1.1.1.1\n/' /etc/resolv.conf
 sudo apt-get -qq update && apt-get -qq upgrade -y
 sudo apt-get -qq install -y iptraf iperf openvpn net-tools snmpd speedtest-cli nano cron ufw snmpd net-tools tmux resolvconf bashtop
 crontab -r
